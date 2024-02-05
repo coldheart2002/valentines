8a062989494fd7720fe457ea7ab5dfa4e1d1a97b
Tampipi Neo <128296315+coldheart2002@users.noreply.github.com>
2024-02-05 20:59:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const nameElement = document.querySelector(".name");
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const name = "Whatever";
+nameElement.innerHTML = `Hi ${name}`;
+
+const MAX_IMAGES = 8;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  nameElement.innerHTML = `${name}`;
+  titleElement.innerHTML = "Yayyy!! :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Are you sure?",
+    "Really sure?",
+    "Just think about it",
+    "You're breaking my heart",
+    "If you say no, I'll be very sad",
+    "I'll be very very sad",
+    "Ok fine, I'll stop asking...",
+    "Just kidding, please say 'YES' :)",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
